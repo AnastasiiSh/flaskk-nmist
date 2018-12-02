7d31d8daca3b444cfca721aac748eede153c7f39
AnastasiiSh <anastasii1@icloud.com>
2018-12-02 19:13:30 +0300

    initial commit

--- static/index.js
+++ static/index.js
@@ -82,3 +82,16 @@
     });
   }
 }());
+
+$(".myButton").click(function(){
+      var $SCRIPT_ROOT = {{ request.script_root|tojson|safe }};
+      var canvasObj = document.getElementById("canvas");
+      var img = canvasObj.toDataURL();
+      $.ajax({
+        type: "POST",
+        url: $SCRIPT_ROOT + "/predict/",
+        data: img,
+        success: function(data){
+          $('#result').text(' Predicted Output: '+data);}
+      });
+    });
